85d07746f6999ad8732c1fd9d7a7a2ae770ca548
Dave Hakkens <mail@davehakkens.nl>
2015-08-10 15:45:56 +0200

    Google analitics
    added Google analytics in the functions.php

--- functions.php
+++ functions.php
@@ -153,4 +153,29 @@ function register_menus() {
 
 add_action( 'after_setup_theme', 'register_menus' );
 
+
+
+
+
+<?php
+add_action('wp_footer', 'add_google_analytics');
+function add_google_analytics() { ?>
+
+// Paste in your Google Analytics tracking code here.
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-48209086-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+<?php } ?>
+
+
+
+
 ?>
