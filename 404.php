69689c2eea69dcf8d49c9a69654e52cf790b2a43
Dave Hakkens <mail@davehakkens.nl>
2015-09-23 19:51:30 +0200

    404 Wordpress original

--- 404.php
+++ 404.php
@@ -1,7 +1,10 @@
 <?php
 /**
  * The template for displaying 404 pages (Not Found)
-
+ *
+ * @package WordPress
+ * @subpackage Twenty_Thirteen
+ * @since Twenty Thirteen 1.0
  */
 
 get_header(); ?>
@@ -10,13 +13,13 @@ get_header(); ?>
 		<div id="content" class="site-content" role="main">
 
 			<header class="page-header">
-				<h1 class="page-title"><?php _e( 'Not Found' ); ?></h1>
+				<h1 class="page-title"><?php _e( 'Not Found', 'twentythirteen' ); ?></h1>
 			</header>
 
 			<div class="page-wrapper">
 				<div class="page-content">
-					<h2><?php _e( 'This is somewhat embarrassing, isn’t it?' ); ?></h2>
-					<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?' ); ?></p>
+					<h2><?php _e( 'This is somewhat embarrassing, isn’t it?', 'twentythirteen' ); ?></h2>
+					<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentythirteen' ); ?></p>
 
 					<?php get_search_form(); ?>
 				</div><!-- .page-content -->
@@ -24,3 +27,5 @@ get_header(); ?>
 
 		</div><!-- #content -->
 	</div><!-- #primary -->
+
+<?php get_footer(); ?>
